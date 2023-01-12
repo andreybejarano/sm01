7fc0cf5a0bbd0e4ad69e20c3022f9c78845e9f69
Andrey Bejarano <andreybejarano@gmail.com>
2023-01-11 21:34:33 -0300

    clase26 statics

--- clase26/controllers/mainController.js
+++ clase26/controllers/mainController.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const controller = {
     index: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/home.html'));
+        res.sendFile(path.join(__dirname, '../views/index.html'));
     }
 };
 
