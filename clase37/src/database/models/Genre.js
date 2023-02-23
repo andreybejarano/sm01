b3ec42cf55dbdf709a6a5dc8fa852d42e25a3a1b
Andrey Bejarano <andreybejarano@gmail.com>
2023-02-22 23:20:40 -0300

    inicio clase 37

---
+++ clase37/src/database/models/Genre.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, dataTypes) => {
+  let alias = 'Genre';
+  let cols = {
+      id: {
+          type: dataTypes.BIGINT(10).UNSIGNED,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true
+      },
+      name: {
+          type: dataTypes.STRING(100),
+          allowNull: false
+      },
+      ranking: {
+          type: dataTypes.INTEGER(10).UNSIGNED,
+          allowNull: false
+      },
+      active: {
+          type: dataTypes.BOOLEAN,
+          allowNull: false
+      }
+  };
+  let config = {
+      tableName: 'genres',
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+  }
+  const Genre = sequelize.define(alias, cols, config);  
+
+  return Genre
+};
